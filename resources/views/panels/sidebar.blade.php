6cf856313ef4718ab1e674cfed6df8ae08033749
NiveaC <a26520008@yahoo.com.tw>
2021-11-02 16:17:06 +0800

    新增不同平台(yt,ig tiktok)

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -35,136 +35,125 @@
       <a class="navbar-toggler" href="javascript:void(0)"><i class="material-icons">menu</i></a></h1>
   </div>
   <!-- 下面頁面選單 -->
-  <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out"
+  <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow sidebar_content" id="slide-out"
     data-menu="menu-navigation" data-collapsible="menu-accordion">
     
-    <li class="navigation-header">
-      <div class="row">
+    <li class="navigation-header sidebar_content">
+      <div class="row sidebar_content">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/dashboard1.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/dashboard1.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Dashboard</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Dashboard</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
       <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/inventory.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/inventory.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Inventories</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Inventories</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
       <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/live.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/live.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Campaign</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Campaign</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
       <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/auto reply.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/auto reply.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Auto Reply</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Auto Reply</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
     <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/payment.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/payment.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Payment</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Payment</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
       <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/regional.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/regional.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Regional</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Regional</a>
         </div>
       </div>
     </li>
     <li class="navigation-header">
       <div class="row">
         <div class="col s2 left p-0">
-          <img class="" src="{{asset('images/lss-icon/settings.png')}}" style="height:14pt; margin-right:-5%;"/>
+          <img class="" src="{{asset('images/lss-icon/settings.png')}}" style="height:14pt;"/>
         </div>
         <div class="col s10">
-          <a href="#" class="navigation-header-text" style="font-size:14px;">Settings</a>
+          <a href="#" class="navigation-header-text sidebar_text" style="font-size:14px; color:rgba(0,0,0,.87);">Settings</a>
         </div>
       </div>
     </li>
-
+    <!-- 平台icon -->
     <div class="row" style="margin-top:5rem;">
       <div class="col s12">
-        <div class="col s3">
-          <img class="" src="{{asset('images/lss-img/facebook.png')}}" style="height:14pt; width:14pt; margin-right:18px; margin-bottom: -0.3rem;"/>
-        </div>
-        <div class="col s3">
-        <img class="" src="{{asset('images/lss-img/youtube.png')}}" style="height:14pt; width:14pt; margin-right:18px; margin-bottom: -0.3rem;"/>
-        </div>
-        <div class="col s3">
-        <img class="" src="{{asset('images/lss-img/instagram.png')}}" style="height:14pt; width:14pt; margin-right:18px; margin-bottom: -0.3rem;"/>
-        </div>
-        <div class="col s3">
-        <img class="" src="{{asset('images/lss-img/tiktok.png')}}" style="height:14pt; width:14pt; margin-right:18px; margin-bottom: -0.3rem;"/>
-        </div>
+        <li class="navigation-header">
+          <div class="row">
+            <div class="col s2 left p-0">
+              <img class="" src="{{asset('images/lss-img/facebook.png')}}" style="height:14pt;"/>
+            </div>
+            <div class="col s10">
+              <a href="#" class="navigation-header-text" style="font-size:14px;">Facebook</a>
+            </div>
+          </div>
+        </li>
+        <li class="navigation-header">
+          <div class="row">
+            <div class="col s2 left p-0">
+              <img class="" src="{{asset('images/lss-img/youtube.png')}}" style="height:14pt;"/>
+            </div>
+            <div class="col s10">
+              <a href="#" class="navigation-header-text" style="font-size:14px;">Youtube</a>
+            </div>
+          </div>
+        </li>
+        <li class="navigation-header">
+          <div class="row">
+            <div class="col s2 left p-0">
+              <img class="" src="{{asset('images/lss-img/instagram.png')}}" style="height:14pt;"/>
+            </div>
+            <div class="col s10">
+              <a href="#" class="navigation-header-text" style="font-size:14px;">Instagram</a>
+            </div>
+          </div>
+        </li>
+        <li class="navigation-header">
+          <div class="row">
+            <div class="col s2 left p-0">
+              <img class="" src="{{asset('images/lss-img/tiktok.png')}}" style="height:14pt;"/>
+            </div>
+            <div class="col s10">
+              <a href="#" class="navigation-header-text" style="font-size:14px;">TikTok</a>
+            </div>
+          </div>
+        </li>
       </div>
     </div>
   </ul>
-  
-    
-  <!-- <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out"
-    data-menu="menu-navigation" data-collapsible="menu-accordion">
-    {{-- Foreach menu item starts --}}
-    @if(!empty($menuData[0]) && isset($menuData[0]))
-      @foreach ($menuData[0]->menu as $menu)
-        @if(isset($menu->navheader))
-        <li class="navigation-header">
-          <a class="navigation-header-text">{{ $menu->navheader }}</a>
-          <i class="navigation-header-icon material-icons">{{$menu->icon }}</i>
-        </li>
-        @else
-        @php
-          $custom_classes="";
-          if(isset($menu->class))
-          {
-          $custom_classes = $menu->class;
-          }
-        @endphp
-      <li class="bold {{(request()->is($menu->url.'*')) ? 'active' : '' }}">
-        <a class="{{$custom_classes}} {{ (request()->is($menu->url.'*')) ? 'active '.$configData['activeMenuColor'] : ''}}"
-          @if(!empty($configData['activeMenuColor'])) {{'style=background:none;box-shadow:none;'}} @endif
-          href="@if(($menu->url)==='javascript:void(0)'){{$menu->url}} @else{{url($menu->url)}} @endif"
-          {{isset($menu->newTab) ? 'target="_blank"':''}}>
-          <i class="material-icons">{{$menu->icon}}</i>
-          <span class="menu-title">{{ __('locale.'.$menu->name)}}</span>
-          @if(isset($menu->tag))
-          <span class="{{$menu->tagcustom}}">{{$menu->tag}}</span>
-          @endif
-        </a>
-          @if(isset($menu->submenu))
-          @include('panels.submenu', ['menu' => $menu->submenu])
-          @endif
-        </li>
-        @endif
-      @endforeach
-    @endif
-  </ul> -->
   <div class="navigation-background"></div>
   <a class="sidenav-trigger btn-sidenav-toggle btn-floating btn-medium waves-effect waves-light hide-on-large-only"
     href="#" data-target="slide-out"><i class="material-icons">menu</i></a>
