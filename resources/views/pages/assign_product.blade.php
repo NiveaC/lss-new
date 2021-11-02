a47c9ec3b79eb36fd72f380802937faf7e46c277
NiveaC <a26520008@yahoo.com.tw>
2021-11-02 09:40:50 +0800

    調整表格細節

--- resources/views/pages/assign_product.blade.php
+++ resources/views/pages/assign_product.blade.php
@@ -21,18 +21,6 @@
 
 {{-- page content --}}
 @section('content')
-<!-- <div class="row campaign_btn">
-  <div class="col s12 m12 l12 pr-0">
-    <div class="col s12 m7 l9 xl7">
-      <a class="waves-effect waves-light btn mt-2 mr-1" style="border-radius:30pt;">Scheduled</a>
-      <a class="waves-effect waves-light btn mt-2 mr-1" style="border-radius:30pt; background-color:#ffffff; color:#ff4081;">History</a>
-    </div>
-    <div class="col s12 m4 offset-m1 l3 xl3 offset-xl2 mt-2 right-align">
-      <a class="waves-effect indigo darken-4 btn mr-1"><i class="material-icons left" style="margin-right:0;">add</i>Create</a>
-      <span><img src="{{asset('images/lss-icon/questions01.png')}}" class="responsive-img right" style="width:30px; height:30px;" alt="Guideline"></span>
-    </div>
-  </div>
-</div> -->
 <div id="assign_info" class="row">
   <div class="col s12">
     <div class="card card-tabs">
@@ -41,15 +29,15 @@
           <div class="row"> 
             <div class="col s12">
               <div class="row">
-                <div class="col s12 m2 l2 input-field">
-                  <select>
+                <div class="col s12 m2 l2 input-field" >
+                  <select class="p-0">
                     <option>Cheese Cake</option>
                     <option>Apple Pie</option>
                     <option>Apple Pie</option>
                     <option>Apple Pie</option>
                     <option>Apple Pie</option>
                   </select>
-                  <label>Name</label>
+                  <label >Name</label>
                 </div>
                 <div class="col s12 m2 l2 input-field">
                   <input id="username" name="username" type="text" class="validate" value="CK"
@@ -118,9 +106,10 @@
                     <th>Campaign Q'ty</th>
                     <th>Max Order Q'ty</th>
                     <th>Price</th>
+                    <th>Edit Item</th>
+                    <th>Delete Item</th>
                     <th>Edit</th>
                     <th>Delete</th>
-                    <th>Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -136,7 +125,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -151,7 +142,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -166,7 +159,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -181,7 +176,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -196,7 +193,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -211,7 +210,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -226,7 +227,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -241,7 +244,9 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
                   <tr>
@@ -256,9 +261,29 @@
                     <td>v</td>
                     <td>
                       <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
-                      <span class="material-icons" style="color:#b6b6b6;">delete</span></span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
                     </td>
                   </tr>
+                  <tr>
+                    <td><img class="responsive-img circle" src="{{asset('images/fruit tart.jpg')}}" width="45px"alt=""></td>
+                    <td>Fruit Tart</td>
+                    <td>FT</td>
+                    <td>Tart</td>
+                    <td>300</td>
+                    <td>25.5</td>
+                    <td>3</td>
+                    <td>v</td>
+                    <td>v</td>
+                    <td>
+                      <span class="material-icons mr-10" style="color:#b6b6b6;">border_color</span>
+                    </td>
+                    <td>
+                      <span class="material-icons" style="color:#b6b6b6;">delete</span>
+                    </td>
+                  </tr>
+                  
                 </tbody>
               </table>
             </div>
